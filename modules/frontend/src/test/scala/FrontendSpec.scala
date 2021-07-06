b82d2d412c71b20c93ee37440761f710cd109d5f
Simon Parten <quafadas@gmail.com>
2021-07-06 17:23:59 +0200

    Make frontend testing not barf

--- modules/frontend/src/test/scala/FrontendSpec.scala
+++ modules/frontend/src/test/scala/FrontendSpec.scala
@@ -2,9 +2,14 @@ package example.frontend
 
 import utest.TestSuite
 import utest.Tests
+import utest.test
 
 object ClientSpec extends TestSuite  {
 
-  override def tests: Tests = ???
+  override def tests: Tests = Tests{
+      test("nothing"){
+          1 == 1
+      }
+  }
 
 }
